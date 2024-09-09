08d1f7aeeb110913bec82e0e5b90f363f37f33df
Jordan Thomas Jesús Barra Durán <132635751+jordanbarraduran@users.noreply.github.com>
2024-09-08 23:01:35 -0300

    Añadida vista de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,11 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
 ];
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonList, IonItem, IonLabel, IonButton, IonInput, IonInputPasswordToggle } from '@ionic/angular/standalone';
+import { ToastController } from '@ionic/angular';
+import { NavigationExtras, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonList, IonItem, IonLabel, IonButton, IonInput, IonInputPasswordToggle]
+})
+export class LoginPage implements OnInit {
+
+  welcomeMessage: string;
+  subMessage: string;
+  username!: string;
+  password!: string;
+
+  constructor(
+    private router: Router,
+    private toastController: ToastController
+  ) { 
+    this.welcomeMessage = "¡Bienvenid@!";
+    this.subMessage = "Nos alegra verte otra vez 😊";
+  }
+
+  validateLogin(){
+    console.log("Ejecutando validacion!")
+    if(this.username === 'admin'
+      && this.password === '12345') {
+      this.showToastMessage('Inicio de sesion valido', 'success')
+      this.welcomeMessage = `Bienvenido ${this.username}`;
+
+      const extras = this.createExtrasUser(this.username);
+      this.router.navigate(['/home'], extras);
+    } else {
+      this.showToastMessage('Inicio de sesion invalido', 'danger')
+    }
+  }
+
+  ngOnInit() {
+  }
+
+  createExtrasUser(u: string): NavigationExtras | undefined {
+    return {
+      state: {
+        user: u
+      }
+    }
+  }
+
+  async showToastMessage(text: string, msgColor: string){
+    const toast = await this.toastController.create({
+      message: text,
+      color: msgColor,
+      position: 'bottom',
+      duration: 3000
+    })
+    toast.present();
+  }
+}
