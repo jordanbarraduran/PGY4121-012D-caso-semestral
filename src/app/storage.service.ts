29c2d585d34b08b9f91c68be000b97c76ecab1da
Atenas <Atekapeko@gmail.com>
2024-10-21 10:22:08 -0300

    Prueba de implementación de service

--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -7,20 +7,27 @@ import { Storage } from '@ionic/storage-angular';
   providedIn: 'root'
 })
 export class StorageService {
-  // Crea una instancia de Storage, como una variable para almacenar datos
+  // Crea una variable de tipo Storage para almacenar datos
   private _storage: Storage | null = null;
 
   // CONSTRUCTOR
   constructor(private storage: Storage) {
-    // 
+    // Inicializa la clase StorageService
     this.init();
   }
 
   // MÉTODOS
   // Inicializa el service
   async init() {
+    // Crea una instancia de la clase Storage
     const storage = await this.storage.create();
-    
 
+    // Asigna el valor a la variable _storage creada anteriormente
+    this._storage = storage;
+  }
+
+  // Método para que los usuarios almacenen valores en el storage
+  public set(key: string, value: string) {
+    
   }
 }
