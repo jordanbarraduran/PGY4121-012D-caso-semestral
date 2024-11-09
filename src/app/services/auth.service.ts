ad48bdb9a118ad9b2d95ef997de23ef0577ada14
Atenas <atekapeko@gmail.com>
2024-11-09 15:07:28 -0300

    Implementar jsonbin

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,30 +14,38 @@ export class AuthService {
   private storage = inject(StorageService);
   private router = inject(Router);
   private profileService = inject(ProfileService);
-  private apiUrl = 'http://localhost:3000/users';
-  
-  constructor() { }
+  private apiUrl =
+    'https://api.jsonbin.io/v3/b/6728e650ad19ca34f8c40232?meta=false';
+
+  constructor() {}
 
   async login(username: string, password: string) {
     try {
       // Get users that match both username and password
       const response = await firstValueFrom(
-        this.http.get<any[]>(`${this.apiUrl}?username=${username}`)
+        this.http.get<{ users: any[] }>(this.apiUrl)
       );
-  
-      // Check if we got a user and if the password matches
-      const user = response.find(u => u.password === password);
-      
+
+      // Get array of users
+      const allUsers = response.users;
+
+      // Checks if user exists and is valid
+      const user = allUsers.find(
+        (u) => u.username === username && u.password === password
+      );
+
+      // If user is valid
       if (user) {
         console.log('User found:', user);
         await this.profileService.updateCurrentUser(user);
         return true;
       }
-      
+
+      // If user is NOT valid
       console.log('Invalid credentials');
       return false;
-  
     } catch (error) {
+      // If login fails
       console.error('Login error:', error);
       return false;
     }
@@ -48,7 +56,7 @@ export class AuthService {
     await this.profileService.clearCurrentUser();
     this.router.navigate(['/login']);
   }
-  
+
   getCurrentUser() {
     console.log('Getting current user');
     return this.storage.getItem('currentUser');
